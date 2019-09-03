358b4c9d911d6ffa437ac95d416201ac2e0e55fa
M. S <10941563+madblade@users.noreply.github.com>
2019-09-03 21:54:59 +0200

    wip

--- src/thoughts-detail/art0.js
+++ src/thoughts-detail/art0.js
@@ -6,7 +6,27 @@ const a1 =
   author: 'm. s', 
   body: [
   	{type: ParagraphType.Title, content: 'Foreword' },
-    {type: ParagraphType.Paragraph, content: 'This is inspired by a sentence by E. Klein.' },
+    {type: ParagraphType.Paragraph, content: 'The realm of moral philosophy is an odd one to venture to: most of the questions raised there are fated to remain partially open. In the following, I explore some interesting ideas inspired by a sentence by E. Klein. I will quote a bit of science fiction, as I think it is a fertile ground for thought experiments. ' },
+
+  	{type: ParagraphType.Title, content: 'I. Kant and Spock'},
+    {type: ParagraphType.Quotation, content: 'The value of principles is greater than the value of values.',
+      origin: 'E. Klein',
+      source: 'Recorded debate about the notion of progress'
+    },
+    {type: ParagraphType.Paragraph, content: 'Principles should be understood as "unbreakable rules" and values as "evaluations" enabling to compare situations (e.g. numerically). This somewhat circular statement by <a href="https://en.wikipedia.org/wiki/Étienne_Klein">Klein</a> illustrates an important conflict. In a given situation requiring one to act, should they stick to their principles no matter what or should they adapt their actions depending on the situation and values at stake? In a 1797 <a href="https://philpapers.org/archive/VARKAL.pdf">debated text</a>, Kant defends a surprising position in the special case of "lying" versus "telling the truth". He opposes, notably, the views of <a href="https://en.wikipedia.org/wiki/Benjamin_Constant">Constant</a>, by saying that one should always tell the truth, even if that should produce unethical consequences. ' },
+    {type: ParagraphType.Paragraph, content: 'Of course, the conflict between values and principles is political at heart. People with diverging political views would answer the question differently. For example, in the <a href="https://en.wikipedia.org/wiki/Star_Trek_III:_The_Search_for_Spock">third Star Trek movie</a>, Spock undergoes a shift in his conception of duty: if initially he believed that the needs of the few outweighed the needs of the many, by the end he declares that the needs of the one outweigh the needs of the many. This is a position in favor of the importance of principles (or, maybe, locality). ' },
+    {type: ParagraphType.Paragraph, content: 'But sticking to one\'s principles does not prove as easy as it seems. In the previous quotation by Klein, the concept of value was used to evaluate the worth of acting by following principles on the one hand, and the worth of acting by optimizing values on the other hand. Similarly, in <a href="https://en.wikipedia.org/wiki/Robot_series_(Asimov)">the Robot series</a> by Asimov, robots are subject to three laws, stated as perfect and unbreakable principles, but respecting these laws depends on solving some form of value optimization (and on interpreting rules). Values are, indeed, everywhere. ' },
+
+  	{type: ParagraphType.Title, content: 'II. A paradox'}, 
+    {type: ParagraphType.Paragraph, content: 'Value optimization problems are of actual importance for our everyday lives, notably with the example of self-driving cars [<a href="https://link.springer.com/chapter/10.1007/978-3-662-45854-9_4">Lin 2016</a>]. To someone who likes values better than principles, enforcing ethics could be seen as solving an optimization problem of this sort. This view raises other issues. ' },
+    {type: ParagraphType.Paragraph, content: 'Thought experiment: optimized value differs at different points in time. Now suppose it diverges. What should you do? Not only a pathological case. ' },
+
+  	{type: ParagraphType.Title, content: 'III. Thinking outside the box'}, 
+    {type: ParagraphType.Paragraph, content: 'Rephrase in terms of locality. ' },
+    {type: ParagraphType.Paragraph, content: 'Faisceaux serre, insulation, moreno graphs and simondon individuation networks ' },
+    {type: ParagraphType.Paragraph, content: 'Mixing multiple networks and multiple localities ' },
+
+
   ]
 }; 
 return a1;      
