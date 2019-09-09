61154fa6e738eb89424c987848adde70264c5105
M. S <10941563+madblade@users.noreply.github.com>
2019-09-09 13:37:09 +0200

    bump

--- src/thoughts.js
+++ src/thoughts.js
@@ -13,37 +13,6 @@ export class Thoughts {
       A2.get(),
       A3.get(),
     
-      { title: 'JavaScript Quines',
-        date: 'draft',
-        body: [
-          {type: ParagraphType.Title, content: 'Introduction' },
-          {type: ParagraphType.Paragraph, content: 'Quines: definition' },
-          {type: ParagraphType.Paragraph, content: 'Problematic' },
-          {type: ParagraphType.Title, content: 'I. Readable quines' },
-          {type: ParagraphType.Code, language: 'javascript',
-              content:
-                '(function quine(){\n' +
-                '  console.log(\'(\' + quine.toString() + \')();\');\n' +
-                '})();'
-            },
-          {type: ParagraphType.Code, language: 'javascript',
-              content:
-                '!function $(){console.log(\'!\'+$+\'()\')}()\n'
-          },
-          {type: ParagraphType.Paragraph, content: 'Discussion' },
-          {type: ParagraphType.Title, content: 'II. Golfed quines' },
-          {type: ParagraphType.Code, language: 'javascript',
-            content:
-              '$=_=>`$=${$};$()`;$()'
-          },
-          {type: ParagraphType.Paragraph, content: 'Discussion' },
-          {type: ParagraphType.Code, language: 'javascript',
-            content:
-              '(_=$=>`(_=${_})()`)()'
-          },
-          {type: ParagraphType.Paragraph, content: 'Discussion' },
-        ]
-      },
       // { title: 'Univ. compression',
       //   date: '2019/01/01',
       //   body: [
