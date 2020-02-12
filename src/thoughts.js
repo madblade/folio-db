3c20590f5964d1cecde5f75d5f536b19ab41c8d9
madblade <merlin@lent.chanteur>
2020-02-12 19:27:59 +0100

    update

--- src/thoughts.js
+++ src/thoughts.js
@@ -1,9 +1,7 @@
 import {ParagraphType} from './article';
-import {AM1} from './thoughts-detail/artM1';
 import {A0} from './thoughts-detail/art0';
 import {A1} from './thoughts-detail/art1';
 import {A2} from './thoughts-detail/art2';
-import {A3} from './thoughts-detail/art3';
 
 export class Thoughts {
 
@@ -12,8 +10,6 @@ export class Thoughts {
       A0.get(),
       A1.get(),
       A2.get(),
-      A3.get(),
-      AM1.get(),
     
       // { title: 'Univ. compression',
       //   date: '2019/01/01',
